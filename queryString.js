c2bac2a9455329f2e66751a2ac20ad99f7f965f6
Marshall Brown <marshallendel@gmail.com>
2023-08-21 16:21:06 -0400

    update functionality

--- queryString.js
+++ queryString.js
@@ -3,8 +3,34 @@
 */
 
 const queryStringParse = (string) => {
+    const result = {}
     const object = new URLSearchParams(string)
-    return   Object.fromEntries(object.entries())
+    try {
+        for([key, value] of object.entries()) {
+            if(value === "") value = true
+            if(result[key]) {
+                result[key] = typeof result[key] === "string" ? result[key] = [result[key], value] : result[key] = [...result[key], value]
+            } else {
+                result[key] = value
+            }
+        }
+        return result
+    } catch(err) {
+        return new TypeError("Incorrect query string format")
+    }
 }
 
-console.log(queryStringParse("?foo=hello&bar=world&zebra=hi"))
+const serializeQueryObject = (object) => {
+    let result = "?"
+   for([key, value] of Object.entries(object)) {
+    if(Array.isArray(value)) {
+        value.forEach(element => result += `${key}=${element}&`)
+    } else {
+        result += `${key}=${value}&`
+    }
+   }
+   return result.slice(0, -1)
+}
+// console.log()
+console.log(serializeQueryObject(queryStringParse("?foo=hello&bar=world&baz&foo=again")))
+
